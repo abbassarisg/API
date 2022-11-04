193f84e86c828d07b260b8a723e76696edc3ae88
Abbas Sarı <abbassari.sg@gmail.com>
2022-11-04 22:33:21 +0300

    04.11.2022API

---
+++ src/test/java/get_requests/Get09.java
@@ -0,0 +1,82 @@
+package get_requests;
+
+import base_url.RestfulBaseUrl;
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+
+public class Get09 extends RestfulBaseUrl {
+
+     /*
+        Given
+            https://restful-booker.herokuapp.com/booking/91
+        When
+            I send GET Request to the url
+        Then
+            Response body should be like that;
+             {
+            {
+    "firstname": "Adam",
+    "lastname": "Macioszek",
+    "totalprice": 400,
+    "depositpaid": true,
+    "bookingdates": {
+        "checkin": "2021-01-01",
+        "checkout": "2021-12-01"
+    },
+    "additionalneeds": "Bathroom"
+}
+            },
+            "additionalneeds": "Breakfast"
+            }
+     */
+
+    @Test
+    public void get09() {
+
+        //Set the url
+
+        spec.pathParams("first","booking","second","91");
+
+
+        //Set the expectedData
+
+        Map<String,String> bookingdatesMap=new HashMap<>();
+        bookingdatesMap.put("checkin","2021-01-01");
+        bookingdatesMap.put("checkout","2021-12-01");
+
+        Map<String,Object> expecteData= new HashMap<>();
+
+        expecteData.put("firstname","Adam");
+        expecteData.put("lastname","Macioszek");
+        expecteData.put("totalprice",400);
+        expecteData.put("depositpaid",true);
+        expecteData.put("bookingdates",bookingdatesMap);
+
+        System.out.println("expecteData = " + expecteData);
+
+        //Send the request and get the reponse
+        Response response=given().spec(spec).when().get("/{first}/{second}");
+        response.prettyPrint();
+
+        //Do assertion
+        Map<String ,Object> actualData=response.as(HashMap.class);
+
+        assertEquals(expecteData.get("firstname"),actualData.get("firstname"));
+        assertEquals(expecteData.get("lastname"),actualData.get("lastname"));
+        assertEquals(expecteData.get("totalprice"),actualData.get("totalprice"));
+        assertEquals(expecteData.get("depositpaid"),actualData.get("depositpaid"));
+        assertEquals(bookingdatesMap.get("checkin"),((Map) (actualData.get("bookingdates"))).get("checkin"));
+        assertEquals(bookingdatesMap.get("checkout"),((Map) (actualData.get("bookingdates"))).get("checkout"));
+
+
+
+
+
+    }
+}
---
+++ src/test/java/testdata/RestfulTestData.java
@@ -0,0 +1,47 @@
+package testdata;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RestfulTestData {
+
+    public Map<String,String > bookigndatesMethod(String checkin,String checkout){
+
+        Map<String,String> bookingdatesMap=new HashMap<>();
+        bookingdatesMap.put("checkin",checkin);
+        bookingdatesMap.put("checkout",checkout);
+
+    return bookingdatesMap;
+
+             }
+
+       public Map<String,Object> expectedDataMethod(String firstname,String lastname,Integer totalprice,Boolean depositpaid,Map<String,String> bookingdates){
+
+
+        Map<String ,Object> expectedData=new HashMap<>();
+
+        expectedData.put("firstname",firstname);
+        expectedData.put("lastname",lastname);
+        expectedData.put("totalprice",totalprice);
+        expectedData.put("depositpaid",depositpaid);
+        expectedData.put("bookingdates",bookingdates);
+
+                    return expectedData;
+        }
+
+      // "           booking": {
+      //              " firstname": "John",
+      //             "lastname": "Doe",
+      //             "totalprice": 11111,
+      //             "depositpaid": true,
+      //             "bookingdates"
+
+
+    public Object objectToMap(Object o){
+
+        return (Map)o;
+    }
+
+    }
+
+
